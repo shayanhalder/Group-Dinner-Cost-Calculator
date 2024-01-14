f99bcfb523fc48bdfa04412e5eb4bfa37b391a1d
Shayan Halder <halders2@uci.edu>
2024-01-13 20:15:20 -0800

    add scrolling to excess accordion content

--- client/src/components/accordion/Accordion.tsx
+++ client/src/components/accordion/Accordion.tsx
@@ -8,6 +8,9 @@ interface AccordionProps {
   customToggler?: ReactNode;
 }
 
+const MAX_ACCORDION_HEIGHT = 50; // vh units
+const MAX_HEIGHT_UNITS = "vh";
+
 export default function Accordion({ buttonTitle, members, children, customToggler = null }: AccordionProps) {
   const [toggled, setToggled] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
@@ -15,31 +18,26 @@ export default function Accordion({ buttonTitle, members, children, customToggle
 
   useEffect(() => {
     if (ref.current && !toggled) {
-      ref.current.style.height = "0px";
+      ref.current.style.maxHeight = "0px";
       ref.current.style.overflow = "hidden";
       setTimeout(() => {
         if (ref.current) ref.current.style.border = "none";
       }, 100);
     } else if (ref.current && toggled) {
-      ref.current.style.overflow = "visible";
-      ref.current.style.height = `${ref.current.scrollHeight + 20}px`;
+      ref.current.style.maxHeight = `${MAX_ACCORDION_HEIGHT}${MAX_HEIGHT_UNITS}`;
+      ref.current.style.overflow = "scroll"; // once past max height, the excess content can be scrolled to
       ref.current.style.border = "2px solid var(--gray-border)";
     }
   }, [toggled]);
 
   useEffect(() => {
-    if (ref.current && membersRef.current.length < members.length && toggled) {
-      ref.current.style.height = `${ref.current.scrollHeight + 20}px`;
-      ref.current.style.overflow = "visible";
-      ref.current.style.border = "2px solid var(--gray-border)";
-      membersRef.current = members;
-    } else if (ref.current && membersRef.current.length > members.length && toggled) {
-      ref.current.style.height = `${ref.current.scrollHeight - 30}px`;
-      ref.current.style.overflow = "visible";
+    if (ref.current && toggled) {
+      ref.current.style.maxHeight = `${MAX_ACCORDION_HEIGHT}${MAX_HEIGHT_UNITS}`;
+      ref.current.style.overflow = "scroll";
       ref.current.style.border = "2px solid var(--gray-border)";
       membersRef.current = members;
     }
-  }, [members, children]);
+  }, [members]);
 
   return (
     <div className={AccordionStyles.parent}>
